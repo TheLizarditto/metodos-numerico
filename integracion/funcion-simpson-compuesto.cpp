97bf9fb38091c2515648b685865bfb59745d64df
Eze E <ezequielsignorini03@gmail.com>
2024-10-15 18:14:17 -0300

    aplico los cambios de simpson a la version con funcion

--- integracion/funcion-simpson-compuesto.cpp
+++ integracion/funcion-simpson-compuesto.cpp
@@ -19,12 +19,12 @@ int main(int argc, char *argv[]) {
     double x;
     double suma = 0;
 
-    for(int i = 1; i < n/2 - 1; i++) {
+    for(int i = 1; i < n - 1; i++) {
         x = a + 2 * i * h;
         suma += 2 * f(x) + 4 * f(x - h);
     }
 
-    suma = h/3 * (4 * f(b - 1) + f(a) + f(b) + suma);
+    suma = h/3 * (f(a) + f(b) + suma);
     printf("El resultado es: %lf", suma);
 
     double error = ( (-1 * pow((b - a), 5)) / (180 * pow(n, 4)) ) * fpppp(); // preguntar que va en fpppp()
